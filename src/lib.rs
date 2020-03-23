6880ce7dc9890c535ae76f6af36dd807fcd92a98
Matthew Schubert <matthew.schubert@shoobs.net>
2020-03-23 21:53:33 +0800

    Sped up a bunch. Still not as fast as Shapely.

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,11 @@
 #![deny(rust_2018_idioms)]
-use ndarray::{s, stack, Axis, Array1, ArrayView2};
+use ndarray::{s, Axis, Array1, Array2, ArrayViewMut1, ArrayView2};
 use numpy::{IntoPyArray, PyArray1, PyArray2};
 use pyo3::prelude::{pymodule, Py, PyModule, PyResult, Python};
 
 #[pymodule]
 fn _rustlib(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    fn rdp(points: ArrayView2<'_, f64>, epsilon: &f64) -> Array1<bool> {
-        if points.is_empty() {
-            return Array1::default((0,));
-        }
-        
+    fn calculate_rdp(points: ArrayView2<'_, f64>, mut mask: ArrayViewMut1<'_, bool>, epsilon: &f64) {        
         // TODO - Maybe creating a Line struct would make sense rather than this math here.
         
         let start = points.slice(s![0, ..]);
@@ -18,44 +14,45 @@ fn _rustlib(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
         // Calculate the unit vector between the start and end points.
         let mut unit: Array1<f64> = &end - &start;
         unit /= unit.dot(&unit).sqrt();
-        
-        let mut result = Array1::zeros((points.len_of(Axis(0)),));
+
+        let end_mids: Array2<f64> = &end.broadcast(points.raw_dim()).unwrap() - &points;
+        let end_mid_2 = (&end_mids * &end_mids).sum_axis(Axis(1));
+        let end_mid_unit = unit.dot(&end_mids.reversed_axes());
+        let distances: Array1<f64> = end_mid_2 - &end_mid_unit * &end_mid_unit;
 
         // Find the point with the maximum distance from the line between the two endpoints.
         let mut d_max = 0.0;
         let mut i_max: usize = 0;
-        let mut distance: f64;
-        for (i, mid) in points.outer_iter().enumerate().take(points.len_of(Axis(0)) - 1).skip(1) {
-            let end_mid = &end - &mid;
-            distance = (end_mid.dot(&end_mid) - end_mid.dot(&unit).powi(2)).sqrt();
-            if distance > d_max {
+        for (i, d) in distances.iter().enumerate().take(distances.len() - 1).skip(1) {
+            if d > &d_max {
                 i_max = i;
-                d_max = distance;
+                d_max = *d;
             }
-            result[i] = distance;
         }
 
         if d_max > *epsilon {
-            let head = rdp(points.slice(s![..=i_max, ..]), epsilon);
-            let tail = rdp(points.slice(s![i_max.., ..]), epsilon);
-            stack(Axis(0), &[head.slice(s![..-1]), tail.view()]).unwrap()
-        } else {
-            let result_length = points.len_of(Axis(0));
-            let mut result = Array1::from_elem((result_length,), false);
-            result[0] = true;
-            result[result_length - 1] = true;
-            result
+            mask[i_max] = true;
+            calculate_rdp(points.slice(s![..=i_max, ..]), mask.slice_mut(s![..=i_max]), epsilon);
+            calculate_rdp(points.slice(s![i_max.., ..]), mask.slice_mut(s![i_max..]), epsilon);
         }
     }
 
     #[pyfn(m, "rdp")]
-    fn rdp_py(
+    fn rdp(
         py: Python<'_>,
         points: &PyArray2<f64>,
         epsilon: f64
     ) -> Py<PyArray1<bool>> {
         let points = points.as_array();
-        rdp(points, &epsilon).into_pyarray(py).to_owned()
+
+        let mut mask = Array1::from_elem((points.len_of(Axis(0)),), false);
+        mask[0] = true;
+        let mask_len = mask.len();
+        mask[mask_len - 1] = true;
+
+        calculate_rdp(points, mask.view_mut(), &epsilon.powi(2));
+
+        mask.into_pyarray(py).to_owned()
     }
 
     Ok(())
