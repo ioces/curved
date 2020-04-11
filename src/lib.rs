66815ad911e40cae1df4158a7215102a6bdab396
Matthew Schubert <matthew.schubert@shoobs.net>
2020-04-11 09:32:53 +0800

    Add a little bit of caching.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
-use ndarray::{s, Axis, Array1, Array2, ArrayViewMut1, ArrayView1, ArrayView2, array};
+use ndarray::{s, Axis, Array1, Array2, ArrayViewMut1, ArrayView1, ArrayView2, CowArray, Ix1, Ix2};
 use numpy::{convert::IntoPyArray, PyArray1, PyArray2};
 use pyo3::prelude::{pymodule, Py, PyModule, PyResult, Python};
+
+
+struct Buffer<'a>(CowArray<'a, f64, Ix2>, CowArray<'a, f64, Ix1>);
+
 
 pub fn rdp(points: ArrayView2<'_, f64>, epsilon: f64) -> Array1<bool> {
     // Generate a mask boolean array, which will be the result.
@@ -10,7 +14,7 @@ pub fn rdp(points: ArrayView2<'_, f64>, epsilon: f64) -> Array1<bool> {
     mask[mask_len - 1] = true;
 
     // Run the recursive RDP algorithm
-    rdp_recurse(points, mask.view_mut(), epsilon);
+    rdp_recurse(points, None, mask.view_mut(), epsilon.powi(2));
 
     mask
 }
@@ -25,15 +29,19 @@ fn line_point_distances(start: ArrayView1<'_, f64>, end: ArrayView1<'_, f64>, po
     (end_mid_2 - &end_mid_unit * &end_mid_unit).mapv(f64::sqrt)
 }
 
-fn line_point_distances_3(start: ArrayView1<'_, f64>, end: ArrayView1<'_, f64>, points: ArrayView2<'_, f64>) -> Array1<f64> {
+fn line_point_distances_3(
+    start: ArrayView1<'_, f64>,
+    end: ArrayView1<'_, f64>,
+    cas: ArrayView2<'_, f64>,
+    mag_cas_2: ArrayView1<'_, f64>,
+    points: ArrayView2<'_, f64>
+) -> Array1<f64> {
     let ab: Array1<f64> = &end - &start;
     let ab_mag = ab.dot(&ab).sqrt();
     let ab_u = ab / ab_mag;
 
-    let cas: Array2<f64> = &start.broadcast(points.raw_dim()).unwrap() - &points;
-    let mag_cas_2 = (&cas * &cas).sum_axis(Axis(1));
-    let mag_ads_2 = ab_u.dot(&cas.reversed_axes()).mapv(|v| v.powi(2));
-    (mag_cas_2 - mag_ads_2).mapv(f64::sqrt)
+    let mag_ads_2 = cas.dot(&ab_u).mapv(|v| v.powi(2));
+    &mag_cas_2 - &mag_ads_2
 }
 
 fn line_point_distances_4(start: ArrayView1<'_, f64>, end: ArrayView1<'_, f64>, points: ArrayView2<'_, f64>) -> Array1<f64> {
@@ -59,12 +67,24 @@ fn line_point_distances_2(start: ArrayView1<'_, f64>, end: ArrayView1<'_, f64>, 
     })
 }
 
-fn rdp_recurse(points: ArrayView2<'_, f64>, mut mask: ArrayViewMut1<'_, bool>, epsilon: f64) {
+fn rdp_recurse(points: ArrayView2<'_, f64>, opt_buffer: Option<Buffer>, mut mask: ArrayViewMut1<'_, bool>, epsilon_2: f64) {
     // Get the start and end points of the curve
     let start = points.slice(s![0, ..]);
     let end = points.slice(s![-1, ..]);
 
-    let distances = line_point_distances_4(start, end, points);
+    let buffer = match opt_buffer {
+        Some(b) => b,
+        None => {
+            let acs = &points - &end;
+            let mag_acs_2 = (&acs * &acs).sum_axis(Axis(1));
+            Buffer(acs.into(), mag_acs_2.into())
+        }
+    };
+    let distances = line_point_distances_3(start, end, buffer.0.view(), buffer.1.view(), points);
+
+    /*
+    let distances = line_point_distances_3(start, end, points);
+    */
 
     // Find the point with the maximum distance from the line joining the endpoints.
     let mut d_max = 0.0;
@@ -79,10 +99,17 @@ fn rdp_recurse(points: ArrayView2<'_, f64>, mut mask: ArrayViewMut1<'_, bool>, e
     // If that point is further away from the line joining the endpoints than epsilon^2,
     // mark it as retained and recurse the algorithm for the line joining start->point
     // and point->end.
-    if d_max > epsilon {
+    if d_max > epsilon_2 {
         mask[i_max] = true;
-        rdp_recurse(points.slice(s![..=i_max, ..]), mask.slice_mut(s![..=i_max]), epsilon);
-        rdp_recurse(points.slice(s![i_max.., ..]), mask.slice_mut(s![i_max..]), epsilon);
+        /*rdp_recurse(points.slice(s![..=i_max, ..]),
+            Some(Buffer(buffer.0.slice(s![..=i_max, ..]).into(), buffer.1.slice(s![..=i_max]).into())),
+            mask.slice_mut(s![..=i_max]),
+            epsilon_2);*/
+        rdp_recurse(points.slice(s![..=i_max, ..]), None, mask.slice_mut(s![..=i_max]), epsilon_2);
+        rdp_recurse(points.slice(s![i_max.., ..]),
+            Some(Buffer(buffer.0.slice(s![i_max.., ..]).into(), buffer.1.slice(s![i_max..]).into())),
+            mask.slice_mut(s![i_max..]),
+            epsilon_2);
     }
 }
 
