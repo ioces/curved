c4e2fec7523af8d7424f704a5d9a240b38c50ab4
Matthew Schubert <matthew.schubert@ioces.com>
2021-09-11 17:38:44 +0200

    Bring into line with new Python packaging style

--- setup.py
+++ setup.py
@@ -2,18 +2,14 @@ from setuptools import find_packages, setup
 from setuptools_rust import Binding, RustExtension
 
 
-setup_requires = ['setuptools-rust>=0.10.2']
 install_requires = ['numpy']
-test_requires = install_requires + ['pytest']
 
 setup(
     name='curved',
-    version='0.1.0',
+    version='0.1.1',
     description='a high performance module for curve simplification',
-    rust_extensions=[RustExtension('curved._rustlib', './Cargo.toml', binding=Binding.PyO3)],
+    rust_extensions=[RustExtension('curved._rustlib', binding=Binding.PyO3)],
     install_requires=install_requires,
-    setup_requires=setup_requires,
-    test_requires=test_requires,
     packages=find_packages(),
     zip_safe=False,
 )
