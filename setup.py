001b1b8d9efd3a5b26ab2e51b053e759684488c9
Matthew Schubert <matthew.schubert@ioces.com>
2021-09-11 18:14:13 +0200

    Use setup.cfg instead of .py

--- setup.py
+++ setup.py
@@ -1,15 +1,6 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
-
-install_requires = ['numpy']
-
 setup(
-    name='curved',
-    version='0.1.1',
-    description='a high performance module for curve simplification',
-    rust_extensions=[RustExtension('curved._rustlib', binding=Binding.PyO3)],
-    install_requires=install_requires,
-    packages=find_packages(),
-    zip_safe=False,
+    rust_extensions=[RustExtension('curved._rustlib', binding=Binding.PyO3)]
 )
